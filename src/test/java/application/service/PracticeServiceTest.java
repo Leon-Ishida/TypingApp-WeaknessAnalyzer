b0a0b437ec6b6b1ef3601ca31a70773fc925e15c
Leon Ishida <leon.ishida.21@gmail.com>
2026-02-09 20:07:32 +0900

    test: Add test case for generating substitution weakness words

--- src/test/java/application/service/PracticeServiceTest.java
+++ src/test/java/application/service/PracticeServiceTest.java
@@ -1,5 +1,78 @@
 package application.service;
 
+import static org.junit.jupiter.api.Assertions.assertAll;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+import static org.mockito.Mockito.when;
+
+import java.time.LocalDateTime;
+import java.util.LinkedHashMap;
+import java.util.List;
+
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.MockitoAnnotations;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+import application.model.MistakeDetail;
+import application.model.MistakeType;
+import application.model.PracticeWords;
+import application.model.TestResult;
+import application.model.WordResult;
+
+@ExtendWith(MockitoExtension.class)
 public class PracticeServiceTest {
-    
+    @Mock
+    private WordManager mockWordManager;
+    private AutoCloseable closeable;
+
+    @InjectMocks
+    private PracticeService practiceService;
+
+    @BeforeEach
+    void initService() {
+        closeable = MockitoAnnotations.openMocks(this);
+    }
+
+    @AfterEach
+    void closeService() throws Exception{
+        closeable.close();
+    }
+
+    @Test
+    @DisplayName("置換ミス: aとsが連続する単語を抽出")
+    void weaknessWordsSubstitution() {
+        List<String> mockDictionary = List.of("assert", "sample", "apple", "sun", "book");
+        when(mockWordManager.getWords()).thenReturn(mockDictionary);
+        when(mockWordManager.getRandomWord()).thenReturn(
+            "dummy1", "dummy2", "dummy3", "dummy4", "dummy5",
+            "dummy7", "dummy8", "dummy9", "dummy10", "dummy11"
+        );
+        List<TestResult> results = createSubstitutionMistake('a', 's');
+        PracticeWords result = practiceService.generatePracticeWords(results);
+        List<String> weaknessWords = result.weaknessWords();
+        assertAll("弱点克服用練習単語の検証(置換ミス)",
+            () -> assertTrue(weaknessWords.contains("assert"), "assertが含まれること"),
+            () -> assertTrue(weaknessWords.contains("sample"), "sampleが含まれること"),
+            () -> assertFalse(weaknessWords.contains("apple"), "appleが含まれないこと"),
+            () -> assertFalse(weaknessWords.contains("sun"), "sunを含まないこと"),
+            () -> assertFalse(weaknessWords.contains("book"), "bookが含まれないこと"),
+            () -> assertEquals(10, weaknessWords.size(), "合計10個であること")
+        );
+
+    }
+
+    private List<TestResult> createSubstitutionMistake(char a1, char a2) {
+        WordResult testWordResult = new WordResult("testWord", List.of(new MistakeDetail(MistakeType.SUBSTITUTION, a1, a2, '\0', 0)));
+        LinkedHashMap<String, WordResult> testResults = new LinkedHashMap<>();
+        testResults.put("testWord", testWordResult);
+        TestResult testResult = new TestResult(LocalDateTime.now(), testResults, 0.0,0.0);
+        return List.of(testResult);
+    }
 }
