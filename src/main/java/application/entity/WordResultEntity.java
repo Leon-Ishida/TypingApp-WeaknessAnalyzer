5842b6fa0ddb3100e8f44ab576bd82fbb42cfbd5
Leon Ishida <leon.ishida.21@gmail.com>
2026-03-22 21:12:16 +0900

    feat: split TestResultEntity into normalized tables with @OneToMany

---
+++ src/main/java/application/entity/WordResultEntity.java
@@ -0,0 +1,46 @@
+package application.entity;
+
+import java.util.List;
+
+import jakarta.persistence.CascadeType;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.OneToMany;
+
+@Entity
+public class WordResultEntity {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @ManyToOne
+    private TestResultEntity testResult;
+
+    @Column(nullable = false)
+    private String word;
+
+    @Column(nullable = false)
+    private String answer;
+
+    @OneToMany(mappedBy = "wordResult", cascade = CascadeType.ALL)
+    private List<MistakeDetailEntity> mistakeDetailEntities;
+
+    protected WordResultEntity() {}
+
+    public WordResultEntity(TestResultEntity testResult, String word, String answer, List<MistakeDetailEntity> mistakeDetailEntities) {
+        this.testResult = testResult;
+        this.word = word;
+        this.answer = answer;
+        this.mistakeDetailEntities = mistakeDetailEntities;
+    }
+
+    public Long getId() { return id; }
+    public TestResultEntity getTestResult() {return testResult; }
+    public String getWord() { return word; }
+    public String getAnswer() { return answer; }
+    public List<MistakeDetailEntity> getMistakeDetailEntities() { return mistakeDetailEntities; }
+}
