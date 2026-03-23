503f5b5820ea8a2b1fd03178f5126708a88028c0
Leon Ishida <leon.ishida.21@gmail.com>
2026-03-23 16:04:13 +0900

    chore: remove learning endpoints and unused Memo entity

--- src/main/java/application/repository/MemoRepository.java
+++
@@ -1,9 +0,0 @@
-package application.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import application.entity.Memo;
-
-public interface MemoRepository extends JpaRepository<Memo, Long> {
-    
-}
