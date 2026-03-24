bade4f96bf43da1f7d48ce501ed714242a78f3ef
Leon Ishida <leon.ishida.21@gmail.com>
2026-03-24 23:41:54 +0900

    refactor: exchange LocalDateTime to long at startedAt in TestStartResponse

--- src/main/java/application/dto/TestStartResponse.java
+++ src/main/java/application/dto/TestStartResponse.java
@@ -1,9 +1,8 @@
 package application.dto;
 
-import java.time.LocalDateTime;
 import java.util.List;
 
 public record TestStartResponse(
     List<String> words,
-    LocalDateTime startedAt
+    long startedAt
 ) {}
