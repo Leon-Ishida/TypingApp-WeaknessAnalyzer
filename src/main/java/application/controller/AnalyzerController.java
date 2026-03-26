de228b23d04cde7a78ded75a2e4dd0b99aa1ae1a
Leon Ishida <leon.ishida.21@gmail.com>
2026-03-26 23:05:23 +0900

    refactor: replace @Autowired with constructor injection in AnalyzerController

--- src/main/java/application/controller/AnalyzerController.java
+++ src/main/java/application/controller/AnalyzerController.java
@@ -9,7 +9,6 @@ import application.service.AnalyzeService;
 import java.util.List;
 import java.util.NoSuchElementException;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -23,18 +22,15 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @RestController
 @RequestMapping("/api")
 public class AnalyzerController {
-    @Autowired
-    private AnalyzeService analyzeService;
+    private final AnalyzeService analyzeService;
+
+    public AnalyzerController(AnalyzeService analyzeService) {
+        this.analyzeService = analyzeService;
+    }
 
     @PostMapping("/analyze")
     public AnalyzeResponse analyzeResult(@RequestBody AnalyzeRequest request) {
         AnalyzeResponse response = analyzeService.analyzeResult(request);
-        return response;
-    }
-
-    @PostMapping("/results")
-    public AnalyzeResponse saveResult(@RequestBody AnalyzeRequest request) {
-        AnalyzeResponse response = analyzeService.saveResult(request);
         return response;
     }
 
